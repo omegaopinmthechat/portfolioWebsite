38c1276e3c7d268cd45753b218bfd6092d4eaefc
Amar <onlyaboutcoc@gmail.com>
2025-07-01 02:41:42 +0530

    added the coding image

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import About from "@/app/about/page";
 import Resume from "@/app/resume/page";
 import Projects from "@/app/projects/page";
+import Image from "next/image";
 
 export default function Home() {
   const aboutRef = useRef<HTMLElement | null>(null);
@@ -16,7 +17,20 @@ export default function Home() {
         scrollToResume={resumeRef}
         scrollToProjects={projectsRef}
       />
-      <div style={{paddingTop: "40px"}}>Main Home about section design</div>
+      <div style={{ paddingTop: "40px" }}>
+        <Image
+          style={{
+            marginTop: "15vh",
+            marginLeft: "110vh",
+            height: "75vh",
+            width: "100vh",
+          }}
+          alt="coding image"
+          width={500}
+          height={300}
+          src="/about.png"
+        />
+      </div>
       {/*For testing the bellow div is created */}
       <div style={{ height: "100vh" }}></div>
       <section
